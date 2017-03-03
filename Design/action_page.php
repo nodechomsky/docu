a654ca4147f2182e65741916af9db8cd36fb8469
William <node@air.local>
2017-03-03 15:31:24 -0600

    with_php_implementation

---
+++ Design/action_page.php
@@ -0,0 +1,46 @@
+ <!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+echo $_GET["Interventions_ENV"]; 
+print "<br>";
+echo $_GET["Interventions_CHA"]; 
+print "<br>";
+echo $_GET["Interventions_RDR"]; 
+print "<br>";
+echo $_GET["Interventions_SCM"]; 
+print "<br>";
+echo $_GET["Interventions_NCR"]; 
+print "<br>";
+echo $_GET["Interventions_BXM"]; 
+print "<br>";
+echo $_GET["Interventions_FPC"]; 
+print "<br>";
+echo $_GET["Interventions_DRX"]; 
+print "<br>";
+echo $_GET["Interventions_RIR"]; 
+print "<br>";
+echo $_GET["Interventions_CTR"]; 
+print "<br>";
+echo $_GET["Interventions_SOD"]; 
+print "<br>";
+echo $_GET["Interventions_BXC"]; 
+print "<br>";
+echo $_GET["Interventions_SHP"]; 
+print "<br>";
+echo $_GET["Interventions_RPL"]; 
+print "<br>";
+echo $_GET["Interventions_SOE"]; 
+print "<br>";
+echo $_GET["Interventions_SRS"]; 
+print "<br>";
+echo $_GET["Interventions_MOD"]; 
+print "<br>";
+echo $_GET["Interventions_PRM"]; 
+print "<br>";
+?>
+
+</body>
+</html> 
